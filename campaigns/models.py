9a2866b42b027cef7db438757ef1ad894c8e68a7
Manoram Agrawal <manoram.agrawal@tanla.com>
2023-06-18 01:52:55 +0530

    Candiadate

--- campaigns/models.py
+++ campaigns/models.py
@@ -5,22 +5,16 @@ from django.contrib.auth.models import User
 class Campaign(models.Model):
     campaign_text = models.CharField(max_length=500)
     created_on = models.DateTimeField(auto_now_add=True)
+    candidates = models.ManyToManyField(User,related_name='campaign_candidates')
+    voters = models.ManyToManyField(User,related_name='campaign_voters')
 
     def __str__(self):
         return self.campaign_text
 
-
-class Choice(models.Model):
-    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.choice_text
-
 class CampaignVote(models.Model):
     campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    candiate = models.ForeignKey(User, on_delete=models.CASCADE,related_name='campaignvote_candiate')
+    voter = models.ForeignKey(User, on_delete=models.CASCADE,related_name='campaignvote_voter')
     created_on = models.DateTimeField(auto_now_add=True)
     class Meta:
         db_table='campaign_vote'
--- campaigns/views.py
+++ campaigns/views.py
@@ -3,18 +3,21 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
-from .models import Campaign, Choice , CampaignVote
+from .models import Campaign, CampaignVote
 from django.http import JsonResponse
 
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.db import transaction
+from users.models import User
+
+from django.db.models import Count
 
 # Get campaigns and display them
 @login_required
 def home(request):
-    campaign_queryset = Campaign.objects.order_by('-created_on')[:5]
-    context = {'campaign_queryset': campaign_queryset}
+    campaign_queryset = Campaign.objects.filter(voters=request.user).order_by('-created_on')
+    context = {'campaign_queryset': campaign_queryset,'len':len(campaign_queryset)}
     return render(request, 'campaigns/index.html', context)
 
 # Show specific campaign and choices
@@ -30,6 +33,9 @@ def detail(request, campaign_id):
 @login_required
 def results(request, campaign_id):
   campaign = get_object_or_404(Campaign, pk=campaign_id)
+  votes=CampaignVote.objects.filter(campaign=campaign).annotate(votes=Count(id)).values('candiate','votes')
+  print(votes)
+
   return render(request, 'campaigns/results.html', { 'campaign': campaign })
 
 # Vote for a campaign choice
@@ -37,25 +43,28 @@ def results(request, campaign_id):
 def vote(request, campaign_id):
     # print(request.POST['choice'])
     campaign = get_object_or_404(Campaign, pk=campaign_id)
-    if CampaignVote.objects.filter(user=request.user,campaign=campaign).exists():
+    if CampaignVote.objects.filter(voter=request.user,campaign=campaign).exists():
        return render(request, 'campaigns/detail.html', {
             'campaign': campaign,
             'error_message': "Already gave the vote.",
         })
     try:
-        selected_choice = campaign.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
+        selected_choice = campaign.candidates.get(pk=request.POST['choice'])
+    except (KeyError, User.DoesNotExist):
         # Redisplay the campaign voting form.
         return render(request, 'campaigns/detail.html', {
             'campaign': campaign,
             'error_message': "You didn't select a choice.",
         })
+    if selected_choice == request.user:
+       return render(request, 'campaigns/detail.html', {
+            'campaign': campaign,
+            'error_message': "Cant Vote for Yourself",
+        })
     else:
-        selected_choice.votes += 1
         try:
           with transaction.atomic():
-            selected_choice.save()
-            CampaignVote.objects.create(user=request.user,campaign=campaign)
+            CampaignVote.objects.create(voter=request.user,campaign=campaign,candiate=selected_choice)
         except Exception as err:
            return render(request, 'campaigns/detail.html', {
             'campaign': campaign,
@@ -69,12 +78,17 @@ def vote(request, campaign_id):
 
 @login_required
 def resultsData(request, obj):
+    print("Coming Here")
     votedata = []
-
     campaign = Campaign.objects.get(id=obj)
-    votes = campaign.choice_set.all()
+    print(campaign)
+    try:
+      votes=CampaignVote.objects.filter(campaign=campaign).values('candiate','id').aggregate(Count(id))
+    except Exception as err:
+       print(err)
+    print(votes)
 
     for i in votes:
         votedata.append({i.choice_text:i.votes})
 
-    return JsonResponse(votedata, safe=False)
+    # return JsonResponse(votedata, safe=False)
